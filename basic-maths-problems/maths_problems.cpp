b16340ff49a62db9bee2ef3ba5dd0842dc9278e7
RidhamSood <ridhamsood1410@gmail.com>
2025-07-19 19:23:40 +0530

    digit count with log method

--- basic-maths-problems/maths_problems.cpp
+++ basic-maths-problems/maths_problems.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 // Extracting the digits one by one 
 int main(){
-    int num;
+    int number,num;
     int digits_count =0;
-    cin>>num;
+    cin>>number;
+    num = number;
     while(num != 0){
         cout<<"digits: "<<num%10<<"\n";
         digits_count += 1;
         num=num/10;
     }
-    cout<<"Total Digits: "<<digits_count;
+    cout<<"Total Digits: "<<digits_count<<"\n";
+    // count the digit
+    cout<<"Total Digits: "<<((int)(log10(number)+1));
     return 0;
 }
