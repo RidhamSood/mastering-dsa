e3a51894c3cb3712221f04f0e979bdb522cad8ca
RidhamSood <ridhamsood1410@gmail.com>
2025-07-21 19:48:07 +0530

    add basic recursion function

--- recursion/recursion.cpp
+++ recursion/recursion.cpp
@@ -1,6 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Just basic implementation of recursion
+int counter = 0;
+void basic_recursion(){
+    counter ++;
+    if(counter == 5){ // condition where function returns and stop calling itself called
+                      // Base Condition
+        return;
+    }
+    else{
+        cout<<counter<<endl;
+        basic_recursion();
+    }
+}
 int main(){
+    basic_recursion();
     return 0;
 }
